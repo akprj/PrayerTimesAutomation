80d54d4e7faf00725b15726a80d37c33c182f82a
N.A.K <noumanahmad1987@gmail.com>
2026-09-25 23:52:05 +0200

    Add calendar events scope to OAuth flow

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ if not REDIRECT_URI.startswith("https://"):
 
 SCOPES = [
     "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/calendar.events",
     "https://www.googleapis.com/auth/userinfo.email",
     "openid",
 ]
