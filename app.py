0b05aa1960620676b0ef71be57243a8fe72be731
N.A.K <noumanahmad1987@gmail.com>
2026-09-26 06:38:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -105,25 +105,26 @@ def health():
 
 @app.route("/connect_google")
 def connect_google():
-session.clear()
-session.permanent = True
+    session.clear()
+    session.permanent = True
 
-config = get_google_client_config()
+    config = get_google_client_config()
 
-if config:
-flow = Flow.from_client_config(
-config,
-scopes=SCOPES,
-autogenerate_code_verifier=True,
-)
-else:
-flow = Flow.from_client_secrets_file(
-CLIENT_SECRETS_FILE,
-scopes=SCOPES,
-autogenerate_code_verifier=True,
-)
+    if config:
+        flow = Flow.from_client_config(
+            config,
+            scopes=SCOPES,
+            autogenerate_code_verifier=True,
+        )
+    else:
+        flow = Flow.from_client_secrets_file(
+            CLIENT_SECRETS_FILE,
+            scopes=SCOPES,
+            autogenerate_code_verifier=True,
+        )
 
-flow.redirect_uri = REDIRECT_URI
+    flow.redirect_uri = REDIRECT_URI
+
 
 authorization_url, state = flow.authorization_url(
 access_type="offline",
